3c6e6f6570655534b1254358ddb3c9c8a85b4e97
shanly <shanly@ticketsolve.com>
2013-03-01 23:17:12 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neworg::Application.config.secret_token = 'a44758233ffc81bd8675ec40e00fe3bac41156debf0447da43e5be0091f15f0ff950dd2c787fd3d80682248936053be593d2188b3dd0dacb899d456d34c81237'
